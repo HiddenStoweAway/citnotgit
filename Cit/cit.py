3bd7df7f9885b28ce15e4b71f3138e103d720133
HiddenStoweAway <124961775+HiddenStoweAway@users.noreply.github.com>
2024-12-04 22:26:29 -0800

    file upload and show commits done

--- Cit/cit.py
+++ Cit/cit.py
@@ -1,5 +1,6 @@
 from Cit import extra_str
 from tinydb import TinyDB, Query
+from termcolor import colored
 
 from datetime import datetime
 
@@ -22,7 +23,7 @@ class cit:
                 tokens.append(wrd)
             elif extra_str.is_numeric(src[0]):
                 number = None
-                while len(src) > 0 and extra_str.is_alphebetic(src[0]):    
+                while len(src) > 0 and (extra_str.is_alphebetic(src[0]) or (len(tokens) > 1 and src[0] == ' ')):    
                     number += src.pop(0)
                     
                 tokens.append(number)
@@ -37,17 +38,32 @@ class cit:
         if(src[0] == 'store'):
             if(len(src) > 1):
                 with open(src[1]) as file:
+                    details = ''
+                    
+                    while len(src) > 2:
+                        details += src.pop(2)
+
                     db.insert(
                         {
                             'Path' : src[1], 
                             'Text' : file.read(),
                             'Date' : datetime.today().__str__(),
-                            'Details' : src[2]
+                            'Details' : details
                         }
                     )
+            else:
+                print("store " + colored("'filepath' 'extra details'", 'yellow'))
         elif(src[0] == 'commits'):
-            for d in db.all():
-                c = d
-                print(c.keys())
-                c.pop('Text')
-                print(c)
+            if db.all():
+                for d in db.all():
+                    c = d
+                    c.pop('Text')
+                    print(c)
+            else:
+                print(colored('No Commits', 'yellow'))
+        elif (src[0] == 'clear'):
+            x = input(colored('Are you sure: Y/n?', 'red'))
+
+            if x.upper() == 'Y':
+                db.truncate()
+                print('Cleared Commits')
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from Cit.cit import cit
+from termcolor import colored
 
 inp = ''
 while inp != 'EXIT':
-    inp = input("Enter your command or type EXIT: ")
+    inp = input("Enter your command or type " + colored('EXIT: ', 'red'))
     citnotgit = cit(inp)
     citnotgit.run()
