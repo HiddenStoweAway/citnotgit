26b26bdbe69e957cc7c880869139c7be2a0bab94
HiddenStoweAway <cashoto13@gmail.com>
2024-12-05 21:54:55 -0500

    finished merging main and working on branches

--- Cit/cit.py
+++ Cit/cit.py
@@ -1,9 +1,6 @@
 from Cit import extra_str
 from tinydb import TinyDB, Query
-<<<<<<< HEAD
-=======
 from termcolor import colored
->>>>>>> 3bd7df7f9885b28ce15e4b71f3138e103d720133
 
 from datetime import datetime
 
@@ -26,11 +23,7 @@ class cit:
                 tokens.append(wrd)
             elif extra_str.is_numeric(src[0]):
                 number = None
-<<<<<<< HEAD
-                while len(src) > 0 and extra_str.is_alphebetic(src[0]):    
-=======
                 while len(src) > 0 and (extra_str.is_alphebetic(src[0]) or (len(tokens) > 1 and src[0] == ' ')):    
->>>>>>> 3bd7df7f9885b28ce15e4b71f3138e103d720133
                     number += src.pop(0)
                     
                 tokens.append(number)
@@ -45,30 +38,16 @@ class cit:
         if(src[0] == 'store'):
             if(len(src) > 1):
                 with open(src[1]) as file:
-<<<<<<< HEAD
-=======
                     details = ''
                     
                     while len(src) > 2:
                         details += src.pop(2)
 
->>>>>>> 3bd7df7f9885b28ce15e4b71f3138e103d720133
                     db.insert(
                         {
                             'Path' : src[1], 
                             'Text' : file.read(),
                             'Date' : datetime.today().__str__(),
-<<<<<<< HEAD
-                            'Details' : src[2]
-                        }
-                    )
-        elif(src[0] == 'commits'):
-            for d in db.all():
-                c = d
-                print(c.keys())
-                c.pop('Text')
-                print(c)
-=======
                             'Details' : details
                         }
                     )
@@ -88,4 +67,3 @@ class cit:
             if x.upper() == 'Y':
                 db.truncate()
                 print('Cleared Commits')
->>>>>>> 3bd7df7f9885b28ce15e4b71f3138e103d720133
